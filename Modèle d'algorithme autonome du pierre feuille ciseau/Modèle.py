0220331f3e8473588ee9f2a5cafe925595c4e4c7
worox780 <louisverite01123@gmail.com>
2024-05-16 22:08:09 +0200

    New

--- Modèle d'algorithme autonome du pierre feuille ciseau/Modèle.py
+++ Modèle d'algorithme autonome du pierre feuille ciseau/Modèle.py
@@ -3,17 +3,31 @@ import random
 victoire_ia = 0
 victoire_joueur = 0
 
+"""
 pierre = 22
 feuille = 9
-sisceau = 10
+ciseau = 10
 joueur = 9
 last_coup = 0
 ia = 14
 
 lst_pierre_suite = [[8, 2, 3], [3, 1, 5], [3, 1, 1], [2, 1, 2], [2, 1, 1], [1, 1, 2]]
 lst_feuille_suite = [[6, 2, 2], [2, 1, 1]]
-lst_sisceau_suite = [[3, 7, 1]]
+lst_ciseau_suite = [[3, 7, 1]]
 suite_de_coup = [2,1] #nb répétition et le coup joué
+"""
+
+pierre = 0
+feuille = 0
+ciseau = 0
+joueur = 0
+last_coup = 0
+ia = 0
+
+lst_pierre_suite = [[1,1,1]]
+lst_feuille_suite = [[1, 1, 1]]
+lst_ciseau_suite = [[1, 1, 1]]
+suite_de_coup = [0,1] #nb répétition et le coup joué
 
 def CoeficientOrientationPierre():
   if (suite_de_coup[1] > len(lst_pierre_suite)): lst_pierre_suite.append([1,1,1])
@@ -21,37 +35,46 @@ def CoeficientOrientationPierre():
 def CoeficientOrientationFeuille():
   if (suite_de_coup[1] > len(lst_feuille_suite)): lst_feuille_suite.append([1,1,1])
 
-def CoeficientOrientationSisceau():
-  if (suite_de_coup[1] > len(lst_sisceau_suite)): lst_sisceau_suite.append([1,1,1])
+def CoeficientOrientationCiseau():
+  if (suite_de_coup[1] > len(lst_ciseau_suite)): lst_ciseau_suite.append([1,1,1])
 
-"""
-Pour p0, pierre concerné par la répétition, la formule est :
-
-p0 = n0/(n*r) avec r la répétition
-p1 = ((1-p0)*n1)/(n1+n2)
-p2 = ((1-p0)*n2)/(n1+n2)
-"""
 
 def Probabilite():
+  global ia
   p0 = 0
   p1 = 0
   p2 = 0
+  #print(ia)
   if suite_de_coup[0] == 0:
     p0 = lst_pierre_suite[suite_de_coup[1]-1][0]/(sum(lst_pierre_suite[suite_de_coup[1]-1])*suite_de_coup[1])
     p1 = ((1-p0)*(lst_pierre_suite[suite_de_coup[1]-1][1]))/(lst_pierre_suite[suite_de_coup[1]-1][1]+lst_pierre_suite[suite_de_coup[1]-1][2])
     p2 = ((1-p0)*(lst_pierre_suite[suite_de_coup[1]-1][2]))/(lst_pierre_suite[suite_de_coup[1]-1][1]+lst_pierre_suite[suite_de_coup[1]-1][2])
+    if ia < p1 * 100: ia = 1 #jouer feuille
+    elif ia < (p0+p1) * 100: ia = 2 #jouer feuille
+    else: ia = 0 #jouer ciseau
+
   elif suite_de_coup[0] == 1:
     p1 = lst_feuille_suite[suite_de_coup[1]-1][1]/(sum(lst_feuille_suite[suite_de_coup[1]-1])*suite_de_coup[1])
     p0 = ((1-p1)*(lst_feuille_suite[suite_de_coup[1]-1][0]))/(lst_feuille_suite[suite_de_coup[1]-1][0]+lst_feuille_suite[suite_de_coup[1]-1][2])
     p2 = ((1-p1)*(lst_feuille_suite[suite_de_coup[1]-1][2]))/(lst_feuille_suite[suite_de_coup[1]-1][0]+lst_feuille_suite[suite_de_coup[1]-1][2])
+    if ia < p0 * 100: ia = 2 #jouer pierre
+    elif ia < (p0+p1) * 100: ia = 0 #jouer feuille
+    else: ia = 1 #jouer siceau
+    
   else:
-    p2 = lst_sisceau_suite[suite_de_coup[1]-1][2]/(sum(lst_sisceau_suite[suite_de_coup[1]-1])*suite_de_coup[1])
-    p0 = ((1-p2)*(lst_sisceau_suite[suite_de_coup[1]-1][0]))/(lst_sisceau_suite[suite_de_coup[1]-1][0]+lst_sisceau_suite[suite_de_coup[1]-1][1])
-    p1 = ((1-p2)*(lst_sisceau_suite[suite_de_coup[1]-1][1]))/(lst_sisceau_suite[suite_de_coup[1]-1][0]+lst_sisceau_suite[suite_de_coup[1]-1][1])
-  
-  return (p0,p1,p2)
-  
+    p2 = lst_ciseau_suite[suite_de_coup[1]-1][2]/(sum(lst_ciseau_suite[suite_de_coup[1]-1])*suite_de_coup[1])
+    p0 = ((1-p2)*(lst_ciseau_suite[suite_de_coup[1]-1][0]))/(lst_ciseau_suite[suite_de_coup[1]-1][0]+lst_ciseau_suite[suite_de_coup[1]-1][1])
+    p1 = ((1-p2)*(lst_ciseau_suite[suite_de_coup[1]-1][1]))/(lst_ciseau_suite[suite_de_coup[1]-1][0]+lst_ciseau_suite[suite_de_coup[1]-1][1])
+    if ia < p0 * 100: ia = 0 #jouer feuille
+    elif ia < (p0+p2) * 100: ia = 1 #jouer ciseau
+    else: ia = 2 #jouer pierre
+    
+  #print(p0, p1, p2)
 
+  #return ia
+  
+#joueur = 0
+#print(Probabilite())
 
 print()
 def Winer(): #Qui gagner
@@ -74,22 +97,23 @@ def Winer(): #Qui gagner
 def SelectionListe(last_coup:int) -> None:
   if last_coup == 0: lst_pierre_suite[suite_de_coup[1]-1][joueur] += 1
   elif last_coup == 1: lst_feuille_suite[suite_de_coup[1]-1][joueur] += 1
-  else: lst_sisceau_suite[suite_de_coup[1]-1][joueur] += 1
+  else: lst_ciseau_suite[suite_de_coup[1]-1][joueur] += 1
 
 
-for i in range(100_000):
+for i in range(50):
   try:
-    joueur = random.randint(0,2) #coup du joueur --> 0 = pierre --> 1 = feuille --> 2 = ciseau
+    joueur = int(input()) #coup du joueur --> 0 = pierre --> 1 = feuille --> 2 = ciseau
   except: break
-  if joueur > 2: break
+  if joueur > 2: joueur = 2
   ia = random.random()*100 #coup de l'ia entre 0 et 100
 
   #----- Section choix du coup de l'ia -----
   #print(Probabilite())
   proba = Probabilite()
-  if ia < proba[1] * 100: ia = 1 #jouer feuille
-  elif ia < (proba[0]+proba[1]) * 100: ia = 2 #jouer ciseau
-  else: ia = 0 #jouer pierre
+  #print(proba)
+  #if ia < proba[1] * 100: ia = 1 #jouer feuille
+  #elif ia < (proba[0]+proba[1]) * 100: ia = 2 #jouer ciseau
+  #else: ia = 0 #jouer pierre
 
   #print(joueur)
   #----- Section pour le traitement du coup du joueur -----
@@ -112,22 +136,21 @@ for i in range(100_000):
       suite_de_coup[0], suite_de_coup[1] = 1, 1
 
   else:
-    sisceau += 1
+    ciseau += 1
     SelectionListe(last_coup=last_coup)
     if suite_de_coup[0] == 2: #Verification si une série est en cours
       suite_de_coup[1] += 1
-      CoeficientOrientationSisceau()
+      CoeficientOrientationCiseau()
     else: #Sinon on modifie les informations de la suite de répétition
       suite_de_coup[0], suite_de_coup[1] = 2, 1
   
   last_coup = joueur
-  Winer()
-  #print()
+  print(Winer())
 
 print(lst_pierre_suite)
 print(lst_feuille_suite)
-print(lst_sisceau_suite)
+print(lst_ciseau_suite)
 
-print(pierre, feuille, sisceau)
+print(pierre, feuille, ciseau)
 
 print(victoire_ia, victoire_joueur)
--- Modèle d'algorithme autonome du pierre feuille ciseau/README.md
+++ Modèle d'algorithme autonome du pierre feuille ciseau/README.md
@@ -8,26 +8,46 @@ Sur un total de 18 tirrages de 100_000 parties chacun, dans lequel le joueur ét
 Les scores des victoires des 18 tirrages de 100_000 parties simulés sont :
 Quand le résultat est négatif, cela signifi que le joueur a gagné. Dans le cas contraire, l'algorithme a gagné.
 
-IA	Joueur	Résultat
-33062	33490	-428
-33284	33483	-199
-33416	33403	13
-33397	33428	-31
-33173	33517	-344
-33416	33149	267
-33381	33013	368
-33294	33327	-33
-33145	33071	74
-33009	33562	-553
-33513	33255	258
-33644	33280	364
-33368	33584	-216
-33670	33050	620
-33377	33114	263
-33502	33162	340
+|IA|Joueur|Résultat|
+| --- | --- | --- |
+|33062|33490|`-428`|
+|33284|33483|`-199`|
+|33416|33403|`13`|
+|33397|33428|`-31`|
+|33173|33517|`-344`|
+|33416|33149|`267`|
+|33381|33013|`368`|
+|33294|33327|`-33`|
+|33145|33071|`74`|
+|33009|33562|`-553`|
+|33513|33255|`258`|
+|33644|33280|`364`|
+|33368|33584|`-216`|
+|33670|33050|`620`|
+|33377|33114|`263`|
+|33502|33162|`340`|
 
 Victoire joueur : 7.
 Victoire algorithme : 9.
 18 tirrages de 100_000 parties chacun.
 
 
+Sur une série de 10 tirrages de 50 parties chacun, simulés contre un vrai joueur, les résultats sont les suivants :
+
+|IA|Joueur|Résultat|
+| --- | --- | --- |
+|15|18|`-3`|
+|22|12|`10`|
+|20|14|`6`|
+|9|11|`-2`|
+|16|17|`-1`|
+|19|15|`4`|
+|13|28|`-5`|
+|15|16|`-1`|
+|21|17|`4`|
+|11|20|`-9`|
+
+Victoire joueur : 6.
+Victoire algorithme : 4.
+Différence totale des écarts de scores finaux : 3. Cela signifie que sur le total de toutes les parties, l'IA a gagné 3 parties de plus avec 10 jeux de données différents. Cela ne démontre en aucun càs l'éfficacité de l'algorithme.
+10 tirrages de 50 parties chacun.
